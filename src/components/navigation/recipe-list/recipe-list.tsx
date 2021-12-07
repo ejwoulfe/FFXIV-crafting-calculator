9fbc8684cd43ac85bbe0c1fe6429ee96b5225411
Ed <edwoulfe@gmail.com>
2021-12-07 14:48:43 -0500

    Replaced the icon property in my sql database with only which disciple folder its in and the recipe name. So now the require.context will provde the initial part of the url while the database will provdie its exact location.

--- src/components/navigation/recipe-list/recipe-list.tsx
+++ src/components/navigation/recipe-list/recipe-list.tsx
@@ -9,29 +9,9 @@ export default function RecipeList(props: { list: RecipeObject[] }) {
 
         const images = require.context('../../../assets/recipe-icons/', true);
         return recipes.map((recipe: RecipeObject, index: number) => {
-            let disciple = "";
 
-            if (recipe.disciple_id === 1) {
-                disciple = "alchemist";
-            } else if (recipe.disciple_id === 2) {
-                disciple = "armorer";
-            } else if (recipe.disciple_id === 3) {
-                disciple = "blacksmith";
-            } else if (recipe.disciple_id === 4) {
-                disciple = "carpenter";
-            } else if (recipe.disciple_id === 5) {
-                disciple = "culinarian";
-            } else if (recipe.disciple_id === 6) {
-                disciple = "goldsmith";
-            } else if (recipe.disciple_id === 7) {
-                disciple = "leatherworker";
-            } else if (recipe.disciple_id === 8) {
-                disciple = "weaver";
-            }
+            let source = images(`./${recipe.icon}`).default;
 
-            let img = recipe.name.replace(/\s+/g, '-').toLowerCase().replace(/\\/g, "/");
-            console.log(img)
-            let source = images(`./${disciple}/${img}.png`).default;
             return (
                 <li className="recipe-list-item" key={"recipe-" + index}>
                     <img src={source} alt={`${recipe.name} icon`} />
